38b7addd79b3c2aef1c6c540bff346531493f0ca
Mohammed Saleh <mohammed_500@outlook.dk>
2025-07-16 21:19:28 +0200

    changes on multible files

--- src/routes/admin-dashboard/+page.server.ts
+++ src/routes/admin-dashboard/+page.server.ts
@@ -1,16 +1,15 @@
 import { redirect, error as svelteKitError } from '@sveltejs/kit';
 import type { ServerLoad } from '@sveltejs/kit';
-/* No additional imports needed since './$types' is empty */
+
 export const load: ServerLoad = async ({ locals }: Parameters<ServerLoad>[0]) => {
   const session = await locals.getSession();
 
   if (!session || !session.user) {
     console.log('Redirecting to /login because session or user is missing');
     throw redirect(303,'/login');
-    
   }
-  const userId = session.user.id;
-  if (userId !== '2c4adab1-ccd9-4827-97ae-0175eca430f6') {
+
+  if (!session.user.user_metadata?.is_super_admin) {
     throw svelteKitError(403, 'Forbidden: You do not have access to this page.');
   }
 
