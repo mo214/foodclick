3b008958bb9e60dc4b350dd5037cbf422b43ab0c
Mohammed Saleh <mohammed_500@outlook.dk>
2025-07-17 16:11:30 +0200

    changes on multible files

--- src/routes/admin-dashboard/+page.svelte
+++ src/routes/admin-dashboard/+page.svelte
@@ -7,8 +7,7 @@
   export let data;
 
   // See what data is passed to the page from your load function
-  console.log('Data available on /admin-dashboard:', data);
-
+  console.log ('da fuuk', data);
   // Defensive parsing
   const user = data?.user as User | undefined;
    const isMasterAdmin = user?.user_metadata?.is_super_admin === true;
